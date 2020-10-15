629ce86ef165cb9ff0e0bdc0a805381225ed8f3a
GxlGemini <2089827424@qq.com>
2020-10-15 23:24:04 +0800

    1

---
+++ src/com/config/demo1.java
@@ -0,0 +1,13 @@
+package com.config;
+
+/**
+ * @Author linxiaobai
+ * @Date 2020/10/15 23:20
+ * @Description TODO
+ * @Version 1.0
+ **/
+public class demo1 {
+    public static void main(String[] args) {
+        System.out.println("sb");
+    }
+}
